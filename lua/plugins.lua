cea5b7bc71f08ef9b9f77075fceaaf51f4d4cede
Benjamin Singelmann <benjamin.singelmann@gmail.com>
2021-05-15 13:43:15 +0200

    Sa 15. Mai 13:24:21 CEST 2021

---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Simple plugins can be specified as strings
+  use '9mm/vim-closer'
+end)
